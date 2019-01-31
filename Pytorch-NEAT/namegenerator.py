a71363ba0f08df4b22c6a7543ffb0ed390cec15d
JoeHarrison <JoeHarrison1994@gmail.com>
2019-01-31 14:45:47 +0100

    Reporter added

--- Pytorch-NEAT/namegenerator.py
+++ Pytorch-NEAT/namegenerator.py
@@ -5,11 +5,13 @@ import numpy as np
 from collections import Counter
 
 class NameGenerator():
-    def __init__(self, csv, markov_order):
+    def __init__(self, csv, markov_order, max_length):
         self.csv = csv
         self.df = pd.read_csv(csv)
 
         self.markov_order = markov_order
+        
+        self.max_length = max_length
 
         self.lowercase_names = self.lowercase_names()
 
@@ -35,7 +37,7 @@ class NameGenerator():
         n = 0
         while True:
             terms = list('<'*self.markov_order)
-            for x in range(np.random.choice(self.name_lengths,1,p=self.name_probabilities)[0]):
+            for x in range(np.clip(np.random.choice(self.name_lengths,1,p=self.name_probabilities)[0],0,self.max_length)):
                 if(self.states[tuple(terms[-self.markov_order:])]):
                     next_letter = choice(self.states[tuple(terms[-self.markov_order:])])
                     terms.append(next_letter)
